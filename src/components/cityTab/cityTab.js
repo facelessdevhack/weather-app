47c6dc1ad55f3d164aeaaf19f6b654bb27e33e7a
facelessdevhack <60548673+facelessdevhack@users.noreply.github.com>
2021-09-13 10:01:58 +0530

    icons Changed

--- src/components/cityTab/cityTab.js
+++ src/components/cityTab/cityTab.js
@@ -1,9 +1,7 @@
-import React, { useEffect} from 'react'
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchWeatherData } from '../../api/weatherData'
+import React from 'react'
+import { useSelector } from 'react-redux';
 
 function CityTab(props) {
-    const dispatch = useDispatch()
     const loaded = useSelector((state) => state.reducer.isLoading)
     const weather = useSelector((state) => state.reducer.weatherData)
     const unique = weather.filter((elem, index) => 
