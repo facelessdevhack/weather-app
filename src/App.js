1d013a5f75784347c9f15bb39430b83ae0d3104f
facelessdevhack <60548673+facelessdevhack@users.noreply.github.com>
2021-09-13 09:56:04 +0530

    Persistance Added

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import Routes from './components/routes/routes';
 import Sidebar from './components/sidebar/sidebar';
 import {  BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import store from './api/store';
+import {store, persistor} from './api/store';
+import { PersistGate } from 'redux-persist/integration/react'
 
 function App() {
   return (
     <div className="App">
       <Provider store={store}>
-        <Router>
-          <Sidebar />
-          <Routes />
-        </Router>
+        <PersistGate loading={null} persistor={persistor}>
+          <Router>
+            <Sidebar />
+            <Routes />
+          </Router>
+        </PersistGate>
       </Provider>
     </div>
   );
--- src/api/store.js
+++ src/api/store.js
@@ -1,5 +1,7 @@
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension'
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'
 import thunk from 'redux-thunk';
 import rootReducer from './index';
 
@@ -7,10 +9,18 @@ const initialState = {};
 
 const middleware = [thunk];
 
+const persistConfig = { // configuration object for redux-persist
+    key: 'root',
+    storage, // define which storage to use
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+
 const store = createStore(
-    rootReducer,
+    persistedReducer,
     initialState,
     composeWithDevTools(applyMiddleware(...middleware))
 );
-
-export default store;
+const persistor = persistStore(store);
+export {store,persistor}
